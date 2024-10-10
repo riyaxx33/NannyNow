17fb71bb02937d5f0b66d0d9e1e20b7cddc75e1e
kybae04 <kyliebay@gmail.com>
2024-10-10 15:27:59 +0800

    Added event listener to redirect user to corresponding home page depending on their profile

--- login.js
+++ login.js
@@ -8,3 +8,39 @@ import {
 // Reference to form and error message
 const loginForm = document.getElementById("loginForm");
 const errorMsg = document.getElementById("error");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // Get user input
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    // Sign in user with Firebase authentication
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+
+    // Check the user's role in Firestore
+    const userDoc = await getDoc(doc(db, "users", user.uid));
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+
+      // Redirect based on user role
+      if (userData.role === "nanny") {
+        window.location.href = "nanny_home.html";
+      } else if (userData.role === "parent") {
+        window.location.href = "parent_home.html";
+      }
+    } else {
+      errorMsg.textContent =
+        "No account found. Please check if your email and password is correct.";
+    }
+  } catch (error) {
+    errorMsg.textContent = error.message;
+  }
+});
