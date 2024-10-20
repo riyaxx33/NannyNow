82b59c6fe2624b9bebd9b402bf3cc8f115a47428
kybae04 <kyliebay@gmail.com>
2024-10-20 18:29:22 +0800

    edited login.js to work with auth.js to handle login for users

--- public/backend/login.js
+++ public/backend/login.js
@@ -1,9 +1,4 @@
-import { auth, db } from "firebase-config.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
-import {
-  doc,
-  getDoc,
-} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { login, redirectBasedOnRole } from "./auth";
 
 // Reference to form and error message
 const loginForm = document.getElementById("loginForm");
@@ -17,29 +12,8 @@ loginForm.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    // Sign in user with Firebase authentication
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const user = userCredential.user;
-
-    // Check the user's role in Firestore
-    const userDoc = await getDoc(doc(db, "users", user.uid));
-    if (userDoc.exists()) {
-      const userData = userDoc.data();
-
-      // Redirect based on user role
-      if (userData.role === "nanny") {
-        window.location.href = "nanny_home.html";
-      } else if (userData.role === "parent") {
-        window.location.href = "parent_home.html";
-      }
-    } else {
-      errorMsg.textContent =
-        "No account found. Please check if your email and password is correct.";
-    }
+    const { user, role } = await login(email, password);
+    redirectBasedOnRole(role);
   } catch (error) {
     errorMsg.textContent = error.message;
   }
